66dd68ba74e20a61e35aadd51d58994a32f307b5
Edelmir Muratkanov <muratkanov02@gmail.com>
2024-08-22 21:03:24 +0500

    feat(api): :sparkles: add swagger

--- api/src/main.ts
+++ api/src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
-import { Logger } from '@nestjs/common'
+import { Logger, ValidationPipe } from '@nestjs/common'
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
 
 async function bootstrap() {
   const logger = new Logger('Bootstrap')
@@ -9,8 +10,21 @@ async function bootstrap() {
 
   app.setGlobalPrefix('api')
 
-  await app.listen(port, async () =>
-    logger.log(`Application started at ${await app.getUrl()}`),
-  )
+  app.useGlobalPipes(new ValidationPipe())
+
+  const config = new DocumentBuilder()
+    .setTitle('Maxinum test')
+    .setVersion('0.1')
+    .addBearerAuth()
+    .build()
+
+  const document = SwaggerModule.createDocument(app, config)
+  SwaggerModule.setup('docs', app, document)
+
+  await app.listen(port, async () => {
+    const appUrl = await app.getUrl()
+    logger.log(`Application started at ${appUrl}`)
+    logger.log(`Swagger at ${appUrl}/docs`)
+  })
 }
 bootstrap()
